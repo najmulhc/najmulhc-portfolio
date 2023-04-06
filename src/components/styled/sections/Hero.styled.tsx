e4bfcf7a1c12150b6be95acbdd693354bb3c0245
Najmul Huda Chowdhury Chayan <87802203+najmulhc@users.noreply.github.com>
2023-04-06 20:11:34 +0600

    hero section set for mobile devices and added favicon and title

--- src/components/styled/sections/Hero.styled.tsx
+++ src/components/styled/sections/Hero.styled.tsx
@@ -4,9 +4,13 @@ import { Card } from "../Card.styled";
 // the card to contain the whole hero section
 export const HeroContainer = styled(Card)`
   margin: 0.5rem 0;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   gap: 2rem;
+  @media only screen and (max-width: 767px) {
+    flex-direction: column-reverse;
+  }
 `;
 
 // the left text part of hero section
@@ -16,34 +20,50 @@ export const HeroTextContainer = styled.div`
   justify-content: center;
   align-items: flex-start;
   margin-left: 2rem;
-  gap: .25rem;
+  gap: 0.25rem;
+  width: 50%;
+  @media only screen and (max-width: 767px) {
+    width: 90%;
+    align-items: center;
+    margin-left: 0;
+    margin-bottom: 2rem;
+  }
 `;
 export const HeroName = styled.h1`
   font-size: 3rem;
   color: #121212;
-  line-height: .9em;
+  line-height: 0.9em;
+  @media only screen and (max-width: 767px) {
+    text-align: center;
+    font-size: 2rem;
+  }
 `;
 export const Name = styled.span`
   color: #ffeb3b;
-
 `;
 export const HeroTitle = styled.h2`
- font-size: 1.25rem;
- font-weight: 500;
- color
+  font-size: 1.25rem;
+  font-weight: 500;
+  @media only screen and (max-width: 767px) {
+    text-align: center;
+    
+  }
 `;
 
 // the right image section of hero
 export const HeroImageContainer = styled.div`
   padding: 2rem 0;
-  width: 100%;
+  width: 50%;
   height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
+  @media only screen and (max-width: 767px) {
+    width: 100%;
+  }
 `;
 export const HeroImage = styled.img`
   height: auto;
-  width: 50%;
+  width: 70%;
   object-fit: contain;
 `;
