c5d256c57f19e2972e5478f23e8c4c511f2856e6
Najmul Huda Chowdhury Chayan <87802203+najmulhc@users.noreply.github.com>
2023-05-30 11:03:58 +0600

    FET - 3 technologies added in the home page

--- app/components/home-sections/TechCard.tsx
+++ app/components/home-sections/TechCard.tsx
@@ -1,26 +1,31 @@
 import Button from "../Button";
-import NextJs from "../Logos/NextJs";
 
-export default function TechCard( ){
-    return (
-      <div className="flex flex-col items-center text-center border border-[#eeeeef] p-4 rounded-md ">
-        <NextJs fill="#eeeeef" width="6rem" />
-        <br />
-        <div className="flex flex-col items-center gap-2 ">
-          <h2 className="text-[#eeeeef] text-2xl font-semibold">Next.js</h2>
-          <p className="px-2 bg-[#eeeeef] text-[.75rem]  text-none rounded-full ">
-            Full stack
-          </p>
-          <p className="text-[#eeeeef] font-normal text-sm ">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat
-            quos eos fuga aspernatur distinctio corporis quas nulla? Vero
-            numquam debitis aliquid velit, est at nobis magni quae molestias
-            voluptatibus sequi!
-          </p>
-          <Button>
-            See Projects on Next.js
-          </Button>
-        </div>
+export interface Technology {
+  name: string;
+  children: React.ReactNode;
+  description: string;
+  techType: String;
+  yearsOfExperience?: number;
+}
+
+export default function TechCard({
+  name,
+  children,
+  techType,
+  description,
+}: Technology) {
+  return (
+    <div className="flex flex-col items-center text-center border border-[#eeeeef] p-4 rounded-md ">
+      {children}
+      <br />
+      <div className="flex flex-col items-center gap-2 ">
+        <h2 className="text-[#eeeeef] text-2xl font-semibold">{name}</h2>
+        <p className="px-2 bg-[#eeeeef] text-[.75rem]  text-none rounded-full ">
+          {techType}
+        </p>
+        <p className="text-[#eeeeef] font-normal text-sm ">{description}</p>
+        <Button>See Projects on {name}</Button>
       </div>
-    );
+    </div>
+  );
 }
--- app/components/home-sections/TechStack.tsx
+++ app/components/home-sections/TechStack.tsx
@@ -1,6 +1,9 @@
 import { FaCode } from "react-icons/fa";
 import SeeMore from "./SeeMore";
 import TechCard from "./TechCard";
+import Javascript from "../Logos/Javascript";
+import NextJs from "../Logos/NextJs";
+import TypeScript from "../Logos/TypeScript";
 export default function TechStack() {
   return (
     <section>
@@ -8,11 +11,30 @@ export default function TechStack() {
       <h1 className="flex items-center justify-center text-4xl font-semibold mt-4 mb-6 text-[#eeeeef] opacity-75 hover:opacity-100 gap-2">
         <FaCode /> Tech Stack
       </h1>
-     <div className="px-10 gap-4 grid grid-cols-1 md:grid-cols-3 mb-4">
-      <TechCard/>
-      <TechCard/>
-      <TechCard/>
-     </div>
+      <div className="px-10 gap-4 grid grid-cols-1 md:grid-cols-3 mb-4">
+        <TechCard
+          name="Next.js"
+          techType="Javascript Framework"
+          description="
+I am currently diving deep into Next.js, the widely embraced framework in the developer community. It has become my go-to tool for creating impressive projects that showcase my creativity and push the boundaries of what's possible."
+        >
+          <NextJs width="5rem" fill="#eeeeef" />{" "}
+        </TechCard>
+        <TechCard
+          name="TypeScript"
+          techType="Programming Language"
+          description="With unwavering confidence, I wholeheartedly embrace TypeScript as an industry favorite, leveraging its might to craft robust, scalable solutions that elevate my development prowess and inspire exceptional project outcomes."
+        >
+          <TypeScript width="5rem" fill="#eeeeef" />{" "}
+        </TechCard>
+        <TechCard
+          name="JavaScript"
+          techType="Programming Language"
+          description="For the past 1.5 years, I've been delving into the fascinating world of JavaScript. With its versatile toolkit, I've crafted captivating personal projects that showcase my creativity and problem-solving skills. It's been an exciting journey of growth and exploration."
+        >
+          <Javascript width="5rem" fill="#eeeeef" />{" "}
+        </TechCard>
+      </div>
       <SeeMore href="/tech-stack">See more</SeeMore>
     </section>
   );
