ec088fe1e1907a32be28993fcf274491e4a08d60
Najmul Huda Chowdhury Chayan <najmulhc@outlook.com>
2023-08-26 14:46:00 +0600

    about me section UI don

--- src/app/components/Pages/Home/Qoute.jsx
+++ src/app/components/Pages/Home/Qoute.jsx
@@ -3,8 +3,8 @@ const Qoute = () => {
   return (
     <section className="px-[128px] bg-[#764ABC] text-white italic text-center py-[48px]">
       <H2>
-       "Everyone in this country should learn how to program <br /> because it teaches
-        you how to think."
+        &qout;Everyone in this country should learn how to program <br />
+        because it teaches you how to think.&qout;
       </H2>
       <H4 classname="text-right pr-[250px]">- Steve Jobs</H4>
     </section>
