a3721c428c42d6e480710bbd8796b5b2543d26a6
Najmul Huda Chowdhury Chayan <najmulhc@outlook.com>
2023-11-04 11:16:20 +0600

    project page added

--- src/app/project/[projectId]/page.jsx
+++ src/app/project/[projectId]/page.jsx
@@ -1,11 +1,24 @@
-import {H1} from "../../components/Typography/Typography"
+import {H1} from "../../components/Typography/Typography";
+import Image from 'next/image';
+import banner from './banner.png'
+import Header from "../../components/Header/Header";
+import PageTitle from '../../components/PageTitle/PageTitle'
 
 const page = () => {
+  const project = {
+    name: "Project", 
+    intro: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis illo assumenda minus provident sit, autem fugiat cum praesentium similique neque doloremque libero laudantium qui aliquam nulla soluta! Fugiat tempore eos, quasi ill"
+  };
+  const {name, intro} = project;
   return (
-    <main> 
-        <H1>Lorem Ipsum dolor sit amet</H1>
-    </main>
-  )
+    <>
+      <Header />
+      <main className="">
+        <Image className="w-full h-auto" src={banner} alt="top banner" />
+         <PageTitle pageName={name} description={intro} />
+      </main>
+    </>
+  );
 };
 
 export default page;
