99d85cef4242dc773e0f20ece9f76c9b1d16d38a
Najmul Huda Chowdhury Chayan <najmulhc@outlook.com>
2023-11-13 11:36:39 +0600

    image added in the hero section

--- src/app/components/Pages/Home/Hero.jsx
+++ src/app/components/Pages/Home/Hero.jsx
@@ -1,17 +1,18 @@
-'use client'
+"use client";
 import { H1, H2, H3, P } from "../../Typography/Typography";
 import Button from "../../Buttons/button";
-
+import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
 import { FaFacebook } from "react-icons/fa6";
 import { useRouter } from "next/navigation";
+import heroImage from "../../../../assets/hero-image.png";
 
 const Hero = () => {
-  const router = useRouter()
+  const router = useRouter();
   return (
-    <section className="w-full min-h-screen flex justify-center items-center flex-col px-[128px] gap-4">
-      <div className="flex justify-center gap-6 items-start w-full h-full flex-col">
-        <H1>Hi, I am Najmul</H1>
+    <section className="w-full md:min-h-screen flex flex-col-reverse md:flex-row md:justify-between md:items-center  md:px-[128px] gap-4">
+      <div className="flex justify-center gap-6 items-center md:items-start w-full h-full flex-col">
+        <H1 classname="md:text-left text-center">Hi, I am Najmul</H1>
         <H3>
           I am a{" "}
           <TypeAnimation
@@ -30,15 +31,12 @@ const Hero = () => {
             wrapper="span"
             cursor={true}
             repeat={Infinity}
-          
           />
         </H3>
         <Button>Download Resume</Button>
       </div>
-      <div>
-        <Button onclick={() => {
-          router.push("https://www.facebook.com")
-        }}> <FaFacebook /></Button>
+      <div className="md:w-[50%] flex justify-center items-center ">
+        <Image className="w-[80%]" src={heroImage} alt="Hero Image " />
       </div>
     </section>
   );
--- src/config/dbconnect.js
+++ src/config/dbconnect.js
@@ -1,16 +1,14 @@
 import mongoose from "mongoose";
 const dbconnect = async () => {
   try {
-    if(!mongoose?.connection?.readyState){
-    await mongoose
-      ?.connect(
+    if (!mongoose.connection.readyState) {
+      await mongoose.connect(
         "mongodb+srv://najmulhudachowdhury:ZMwX2guxAjauZMGZ@portfoliocluster.pzx1mdh.mongodb.net/?retryWrites=true&w=majority"
       );
       console.log("Connected with mongodb");
     } else {
-        console.log("Mongodb already connected!");
+      console.log("Mongodb already connected!");
     }
-   
   } catch (error) {
     console.log("Mongodb connection error", error.message);
   }
